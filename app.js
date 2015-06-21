2ec82001241d8c2f51700afdac313a59130d7976
TKais <TiffanyTKaiser@gmail.com>
2015-06-21 09:49:47 -0700

    Get response back from Yoda API

--- app.js
+++ app.js
@@ -7,17 +7,25 @@ var server = app.listen(8080, function(){
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Yodafy listening at http://%s:%s', host, port);
 
 });
 
 
-app.get('/', function(req, res){
+function callAPI(){
   unirest.get("https://yoda.p.mashape.com/yoda?sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait.")
     .header("X-Mashape-Key", "")
     .header("Accept", "text/plain")
     .end(function (result) {
       console.log(result.status, result.headers, result.body);
-    }
-});
+      var newBody = result.body;
+      doSomething(newBody);
+  })
+};
+
+function doSomething(newBody){
+  console.log(newBody);
+}
+
+callAPI();
 
